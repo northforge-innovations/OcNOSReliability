725d0e0409b94581e39353ae75cc0ba17611ea26
Vadim Suraev <vsuraev@northforgeinc.com>
2019-04-18 21:18:42 -0400

    custom allocator skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -295,3 +295,27 @@ pub extern "C" fn peer_route_delete(_peer_prefix: u32, _route_prefix: u32) -> i3
         -1
     }
 }
+
+use std::alloc::{GlobalAlloc, System, Layout};
+
+extern "C" {
+    fn pool_alloc(size: usize) -> *mut u8;
+    fn pool_free(ptr: *mut u8);
+}
+
+struct MyAllocator;
+
+unsafe impl GlobalAlloc for MyAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        //System.alloc(layout)
+	pool_alloc(layout.size())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        //System.dealloc(ptr, layout)
+	pool_free(ptr);
+    }
+}
+
+#[global_allocator]
+static GLOBAL: MyAllocator = MyAllocator;
